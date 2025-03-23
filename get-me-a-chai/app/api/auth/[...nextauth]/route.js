fe804473c2d54ad682879baa2e0b9ea03e5deb04
Varun <varunbalwani123@gmail.com>
2025-03-23 13:26:33 +0530

    github oauth authentication added

---
+++ get-me-a-chai/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+const authoptions = NextAuth({
+  providers: [
+    // OAuth authentication providers...
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  //   secret: process.env.NEXTAUTH_SECRET,
+});
+
+export { authoptions as GET, authoptions as POST };
--- get-me-a-chai/components/SessionWrapper.js
+++ get-me-a-chai/components/SessionWrapper.js
@@ -1,6 +1,7 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
 
-export default function SessionWrapper({ children }) {
+const SessionWrapper = ({ children }) => {
   return <SessionProvider>{children}</SessionProvider>;
-}
+};
+export default SessionWrapper;
